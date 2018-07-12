3b67f16ed72d9265bed5a1c58f3931da22c4f991
Suguru Motegi <suguru.motegi@gmail.com>
2018-07-12 15:38:15 -0700

    refactor(parser-typescript): move to hooks directory

--- hooks/parser-typescript.js
+++ hooks/parser-typescript.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 
-const utils = require('../../utils');
+const utils = require('../utils');
 
 const { dirname } = utils.prettier;
 const parserpath = path.join(dirname, 'parser-typescript');
